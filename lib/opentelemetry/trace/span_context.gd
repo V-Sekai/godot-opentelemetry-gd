1d8c175ab2fd596ed1add8361c4d2ce3b2e745b5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-06-08 16:13:52 -0700

    Implement tracestate.

--- lib/opentelemetry/trace/span_context.gd
+++ lib/opentelemetry/trace/span_context.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var TraceState = preload("res://path/to/opentelemetry/trace/tracestate.gd")
+var TraceState = preload("tracestate.gd")
 
 const INVALID_TRACE_ID = "00000000000000000000000000000000"
 const INVALID_SPAN_ID = "0000000000000000"
@@ -12,32 +12,32 @@ var _trace_state : Object
 var _remote : bool
 
 func init(tid: String, sid: String, trace_flags: int, trace_state: Object = null, remote: bool = false):
-    _trace_id = tid
-    _span_id = sid
-    _trace_flags = trace_flags
-    _trace_state = trace_state if trace_state else TraceState.new({})
-    _remote = remote
+	_trace_id = tid
+	_span_id = sid
+	_trace_flags = trace_flags
+	_trace_state = trace_state if trace_state else TraceState.new([])
+	_remote = remote
 
 func is_valid() -> bool:
-    if _trace_id == INVALID_TRACE_ID or _trace_id == "":
-        return false
+	if _trace_id == INVALID_TRACE_ID or _trace_id == "":
+		return false
 
-    if _span_id == INVALID_SPAN_ID or _span_id == "":
-        return false
+	if _span_id == INVALID_SPAN_ID or _span_id == "":
+		return false
 
-    return true
+	return true
 
 func is_remote() -> bool:
-    return _remote
+	return _remote
 
 func is_sampled() -> bool:
-    return (_trace_flags & 1) == 1
+	return (_trace_flags & 1) == 1
 
 func plain() -> Dictionary:
-    return {
-        "trace_id": _trace_id,
-        "span_id": _span_id,
-        "trace_flags": _trace_flags,
-        "trace_state": _trace_state,
-        "remote": _remote
-    }
+	return {
+		"trace_id": _trace_id,
+		"span_id": _span_id,
+		"trace_flags": _trace_flags,
+		"trace_state": _trace_state,
+		"remote": _remote
+	}
--- lib/opentelemetry/trace/tracestate.gd
+++ lib/opentelemetry/trace/tracestate.gd
@@ -1,5 +1,5 @@
 class_name TraceState
-extends Reference
+extends RefCounted
 
 const MAX_KEY_LEN = 256
 const MAX_VAL_LEN = 256
@@ -8,91 +8,97 @@ const MAX_ENTRIES = 32
 var values : Array
 
 func _init(values: Array):
-    self.values = values
+	self.values = values
 
 static func validate_member_key(key: String) -> String:
-    if key.length() > MAX_KEY_LEN:
-        return ""
+	if key.length() > MAX_KEY_LEN:
+		return String()
 
-    var valid_key = key.strip_edges().match("^([a-z][_0-9a-z%-*/]*)$")
-    if not valid_key:
-        var tenant_id = key.strip_edges().match("^([a-z0-9][_0-9a-z%-*/]*)@([a-z][_0-9a-z%-*/]*)$")
-        if not tenant_id or tenant_id.size() != 2 or tenant_id[0].length() > 241 or tenant_id[1].length() > 14:
-            return ""
-        return tenant_id[0] + "@" + tenant_id[1]
+	var valid_key = key.strip_edges().match("^([a-z][_0-9a-z%-*/]*)$")
+	if valid_key:
+		return key
 
-    return valid_key
+	var tenant_id = key.strip_edges().match("^([a-z0-9][_0-9a-z%-*/]*)@([a-z][_0-9a-z%-*/]*)$")
+	if tenant_id and key.find("@") != -1:
+		var split_key = key.split("@")
+		if split_key.size() == 2 and split_key[0].length() <= 241 and split_key[1].length() <= 14:
+			return split_key[0] + "@" + split_key[1]
+
+	return ""
 
 static func validate_member_value(value: String) -> String:
-    if value.length() > MAX_VAL_LEN:
-        return ""
-    return value.match("^([ !\"#$%%&'()*+%-./0-9:;<>?@A-Z[\%]^_`a-z{|}~]*[!\"#$%%&'()*+%-./0-9:;<>?@A-Z[\%]^_`a-z{|}~])\s*$")
+	if value.length() > MAX_VAL_LEN:
+		return ""
 
-static func new(values: Array) -> TraceState:
-    return TraceState.new(values)
+	var valid_value = value.match("^([ !\"#$%&'()*+,-./0-9:;<=>?@A-Z[\\]^_`a-z{|}~]*[!\"#$%&'()*+,-./0-9:;<=>?@A-Z[\\]^_`a-z{|}~])\\\\s*$")
+	if valid_value:
+		return value
+
+	return ""
+
 
 static func parse_tracestate(tracestate) -> TraceState:
-    if tracestate == null:
-        return TraceState.new([])
+	if tracestate == null:
+		return TraceState.new([])
 
-    if typeof(tracestate) == TYPE_STRING:
-        tracestate = [tracestate]
+	if typeof(tracestate) == TYPE_STRING:
+		tracestate = [tracestate]
 
-    var new_tracestate = []
-    var members_count = 0
-    var error_message = "failed to parse tracestate"
-    for item in tracestate:
-        for member in item.split(","):
-            if member != "":
-                var start_pos = member.find("=")
-                if start_pos == -1 or start_pos == 0:
-                    print(error_message)
-                    return TraceState.new([])
-                var key = validate_member_key(member.left(start_pos))
-                if key == "":
-                    print(error_message)
-                    return TraceState.new([])
-                var value = validate_member_value(member.right(member.length() - start_pos - 1))
-                if value == "":
-                    print(error_message)
-                    return TraceState.new([])
-                members_count += 1
-                if members_count > MAX_ENTRIES:
-                    print(error_message)
-                    return TraceState.new([])
-                new_tracestate.append([key, value])
-    return TraceState.new(new_tracestate)
+	var new_tracestate = []
+	var members_count = 0
+	var error_message = "failed to parse tracestate"
+	for item in tracestate:
+		for member in item.split(","):
+			if member != "":
+				var start_pos = member.find("=")
+				if start_pos == -1 or start_pos == 0:
+					print(error_message)
+					return TraceState.new([])
+				var key = validate_member_key(member.left(start_pos))
+				if key == "":
+					print(error_message)
+					return TraceState.new([])
+				var value = validate_member_value(member.right(member.length() - start_pos - 1))
+				if value == "":
+					print(error_message)
+					return TraceState.new([])
+				members_count += 1
+				if members_count > MAX_ENTRIES:
+					print(error_message)
+					return TraceState.new([])
+				new_tracestate.append([key, value])
+	return TraceState.new(new_tracestate)
 
-func set(key: String, value: String) -> TraceState:
-    if validate_member_key(key) == "":
-        return self
-    if validate_member_value(value) == "":
-        return self
-    del(key)
-    if values.size() >= MAX_ENTRIES:
-        values.pop_back()
-        print("tracestate max values exceeded, removing rightmost entry")
-    values.push_front([key, value])
-    return self
+func set_value(key: String, value: String) -> TraceState:
+	if validate_member_key(key) == "":
+		return self
+	if validate_member_value(value) == "":
+		return self
+	del_value(key)
+	if values.size() >= MAX_ENTRIES:
+		values.pop_back()
+		print("tracestate max values exceeded, removing rightmost entry")
+	values.push_front([key, value])
+	return self
 
-func get(key: String) -> String:
-    for item in values:
-        if item[0] == key:
-            return item[1]
-    return ""
+func get_value(key: String) -> String:
+	for item in values:
+		if item[0] == key:
+			return item[1]
+	return ""
 
-func del(key: String) -> TraceState:
-    var index = -1
-    for i in range(values.size()):
-        if values[i][0] == key:
-            index = i
-            break
-    if index != -1:
-        values.remove(index)
-    return self
+func del_value(key: String) -> TraceState:
+	var index = -1
+	for i in range(values.size()):
+		if values[i][0] == key:
+			index = i
+			break
+	if index != -1:
+		values.remove_at(index)
+	return self
 
 func as_string() -> String:
-    var output = []
-    for item in values:
-        output.append(item[0] + "=" + item[1])
-    return ",".join(output)
+	var output = []
+	for item in values:
+		output.append(item[0] + "=" + item[1])
+	return ",".join(output)
